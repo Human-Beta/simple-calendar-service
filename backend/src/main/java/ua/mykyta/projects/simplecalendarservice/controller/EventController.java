f68c553a0d3ae446c338f138cfb81d1936af6e46
Mykyta Shishov <workmailnikita@gmail.com>
2023-10-30 20:09:07 +0200

    backend: add cors config

--- backend/src/main/java/ua/mykyta/projects/simplecalendarservice/controller/EventController.java
+++ backend/src/main/java/ua/mykyta/projects/simplecalendarservice/controller/EventController.java
@@ -2,6 +2,7 @@ package ua.mykyta.projects.simplecalendarservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/events")
 public class EventController {
 
